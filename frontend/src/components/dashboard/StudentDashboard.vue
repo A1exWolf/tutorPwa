6eb601cf5c61fbf4326a1561577420ce363095ca
Dont't worry <81536837+A1exWolf@users.noreply.github.com>
2025-03-25 08:54:06 +0300

    Update StudentDashboard.vue

--- frontend/src/components/dashboard/StudentDashboard.vue
+++ frontend/src/components/dashboard/StudentDashboard.vue
@@ -88,7 +88,7 @@ import { ref, onMounted } from "vue";
 import { useAuthStore } from "../../stores/auth";
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api";
+const API_URL = "/api";
 const authStore = useAuthStore();
 
 const completedSubmissions = ref([]);
